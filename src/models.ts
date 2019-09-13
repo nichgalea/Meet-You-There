8d3585b312114baaa669999eab2ff8d00ca7358c
Nicholas Galea <nich.galea@gmail.com>
2019-09-13 19:21:42 +0200

    Put Foursquare interface under namespace

--- src/models.ts
+++ src/models.ts
@@ -3,125 +3,127 @@
  *  `/venues/explore` endpoint, then renamed certain interfaces accordingly.
  */
 
-interface Recommendation {
-  meta: RecommendationResponseMetadata;
-  response: RecommendationResponse;
-}
+export namespace Foursquare {
+  export interface Recommendation {
+    meta: RecommendationResponseMetadata;
+    response: RecommendationResponse;
+  }
 
-interface RecommendationResponse {
-  suggestedFilters: SuggestedFilters;
-  suggestedRadius: number;
-  headerLocation: string;
-  headerFullLocation: string;
-  headerLocationGranularity: string;
-  query: string;
-  totalResults: number;
-  suggestedBounds: SuggestedBounds;
-  groups: Group[];
-}
+  export interface RecommendationResponse {
+    suggestedFilters: SuggestedFilters;
+    suggestedRadius: number;
+    headerLocation: string;
+    headerFullLocation: string;
+    headerLocationGranularity: string;
+    query: string;
+    totalResults: number;
+    suggestedBounds: SuggestedBounds;
+    groups: Group[];
+  }
 
-interface Group {
-  type: string;
-  name: string;
-  items: GroupItem[];
-}
+  export interface Group {
+    type: string;
+    name: string;
+    items: GroupItem[];
+  }
 
-interface GroupItem {
-  reasons: Reasons;
-  venue: Venue;
-  referralId: string;
-  flags?: Flags;
-}
+  export interface GroupItem {
+    reasons: Reasons;
+    venue: Venue;
+    referralId: string;
+    flags?: Flags;
+  }
 
-interface Flags {
-  outsideRadius: boolean;
-}
+  export interface Flags {
+    outsideRadius: boolean;
+  }
 
-interface Venue {
-  id: string;
-  name: string;
-  location: Location;
-  categories: Category[];
-  photos: Photos;
-  venuePage?: VenuePage;
-}
+  export interface Venue {
+    id: string;
+    name: string;
+    location: Location;
+    categories: Category[];
+    photos: Photos;
+    venuePage?: VenuePage;
+  }
 
-interface VenuePage {
-  id: string;
-}
+  export interface VenuePage {
+    id: string;
+  }
 
-interface Photos {
-  count: number;
-  groups: any[];
-}
+  export interface Photos {
+    count: number;
+    groups: any[];
+  }
 
-interface Category {
-  id: string;
-  name: string;
-  pluralName: string;
-  shortName: string;
-  icon: Icon;
-  primary: boolean;
-}
+  export interface Category {
+    id: string;
+    name: string;
+    pluralName: string;
+    shortName: string;
+    icon: Icon;
+    primary: boolean;
+  }
 
-interface Icon {
-  prefix: string;
-  suffix: string;
-}
+  export interface Icon {
+    prefix: string;
+    suffix: string;
+  }
 
-interface Location {
-  address?: string;
-  lat: number;
-  lng: number;
-  labeledLatLngs: LabeledLatLng[];
-  distance: number;
-  postalCode?: string;
-  cc: string;
-  city: string;
-  state: string;
-  country: string;
-  formattedAddress: string[];
-  crossStreet?: string;
-}
+  export interface Location {
+    address?: string;
+    lat: number;
+    lng: number;
+    labeledLatLngs: LabeledLatLng[];
+    distance: number;
+    postalCode?: string;
+    cc: string;
+    city: string;
+    state: string;
+    country: string;
+    formattedAddress: string[];
+    crossStreet?: string;
+  }
 
-interface LabeledLatLng {
-  label: string;
-  lat: number;
-  lng: number;
-}
+  export interface LabeledLatLng {
+    label: string;
+    lat: number;
+    lng: number;
+  }
 
-interface Reasons {
-  count: number;
-  items: Reason[];
-}
+  export interface Reasons {
+    count: number;
+    items: Reason[];
+  }
 
-interface Reason {
-  summary: string;
-  type: string;
-  reasonName: string;
-}
+  export interface Reason {
+    summary: string;
+    type: string;
+    reasonName: string;
+  }
 
-interface SuggestedBounds {
-  ne: Ne;
-  sw: Ne;
-}
+  export interface SuggestedBounds {
+    ne: Ne;
+    sw: Ne;
+  }
 
-interface Ne {
-  lat: number;
-  lng: number;
-}
+  export interface Ne {
+    lat: number;
+    lng: number;
+  }
 
-interface SuggestedFilters {
-  header: string;
-  filters: Filter[];
-}
+  export interface SuggestedFilters {
+    header: string;
+    filters: Filter[];
+  }
 
-interface Filter {
-  name: string;
-  key: string;
-}
+  export interface Filter {
+    name: string;
+    key: string;
+  }
 
-interface RecommendationResponseMetadata {
-  code: number;
-  requestId: string;
+  export interface RecommendationResponseMetadata {
+    code: number;
+    requestId: string;
+  }
 }
