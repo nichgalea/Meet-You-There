ad3dc23e04d657575719229311dfe7664be7de02
Nicholas Galea <nich.galea@gmail.com>
2019-09-13 18:35:10 +0200

    Fix unit test

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,13 +1,10 @@
 import { expect } from "chai";
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import SearchContainer from "@/components/HelloWorld.vue";
 
-describe("HelloWorld.vue", () => {
+describe("SearchContainer.vue", () => {
   it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    });
-    expect(wrapper.text()).to.include(msg);
+    const wrapper = shallowMount(SearchContainer);
+    expect(wrapper.text()).to.not.be.null;
   });
 });
